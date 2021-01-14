eca000dbeeefddf3408053b1b247f7a86d277412
Mustafa <startmk2020@gmail.com>
2021-01-14 15:14:58 +0100

    partie 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Logo1 from './googLogo.png';
+// import Logo2 from './img/google.png';
+
 
 function App() {
 
   return (
 
     <div className="App">
-
+      <img src={Logo1} alt=""/>
+      <img src="./img/google.png" alt=""/>
       <p>c'est ici que je vais coder </p>
       
     </div>
